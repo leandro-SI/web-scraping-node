c0ede7b1099d017e93284ecda372f1e84d7c48d4
Leandro <leandro.almeida@uvvnet.com.br>
2023-11-21 12:19:28 -0300

    front listar hunteds

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./src/router');
 
 const app = express();
 
+app.use(cors());
 app.use(router);
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 app.listen(3000, () => {
     console.log('Servidor está funcionando...')
---
+++ front/js/tabela.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    carregarTabela();
+});
+
+
+
+
+
+
+function carregarTabela() {
+    $.ajax({
+        url: 'http://localhost:3000/hunteds',
+        method: 'GET',
+        success: function(data) {
+          console.log('Data from the server:', data);
+
+          $('.total').text(data.total);
+
+          var tbody = $('.table-hunteds tbody');
+          tbody.empty();
+
+          $.each(data.data, function(index, hunted) {
+            var row = $('<tr>');
+            row.append('<td>' + hunted.nome + '</td>');
+            row.append('<td>' + hunted.level + '</td>');
+            row.append('<td>' + hunted.profissao + '</td>');
+            tbody.append(row);
+          });
+
+        },
+        error: function(error) {
+          console.error('Error:', error);
+        }
+      });
+}
